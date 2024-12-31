44e3058ab477b144d11610b018c4f77f137cf01f
Huynh Gia Bao Nguyen <h.nguyen@stud.elka.pw.edu.pl>
2024-12-31 19:58:55 +0100

    updating utils header comes with utils source code

--- T4/utils.h
+++ T4/utils.h
@@ -6,6 +6,16 @@
 
 void ClearConsole(); // Clears the console screen
 void WaitForEnter(); // Waits for user to press Enter
+
+int askForPlayers(int numPlayer); /*
+    int numPlayer; number of players
+    Function asks user for the number of players
+*/
+void initializePlayers(int numPlayer, Player players[]);/*
+    int numPlayer; number of players
+    Player players[]; array of players containing their scores
+    Function initializes players with their respective scores
+*/
 void AskForDimensions(Board* board);/*
     Board* board; pointer to the structure board(containing rows,columns,pointer to 2D matrix, penguins_per_player)
     Function asks user for dimensions of the board 
@@ -14,8 +24,9 @@ void AskForPenguins(Board* board);/*
     Board* board; pointer to the structure board(containing rows,columns,pointer to 2D matrix, penguins_per_player)
     Function asks user fo the amount of penguins each player should have
 */
-int ValidateDimensionsAndPenguins(Board* board);/*
+int ValidateDimensionsAndPenguins(Board* board, int numPlayer);/*
     Board* board; pointer to the structure board(containing rows,columns,pointer to 2D matrix, penguins_per_player)
+    int numPlayer; number of players
     Function validates the dimensions and total amount of penguins on the board
     returns 1 if dimensions and amount of penguins are in order
     returns 0 otherwise
@@ -47,11 +58,20 @@ void AskForCoordinatesMovement(int *x, int *y, int *x1,int *y1,int sign, Board* 
     Board* board; pointer to the structure board(containing rows,columns,pointer to 2D matrix, penguins_per_player)
     Function asks user where to move earlier specified penguin
 */
+void printScores(Player players[], int numPlayer);/*
+    Player players[]; array of players containing their scores and player_id
+    int numPlayer; number of players
+    Function prints the scores of each player
+*/
+void summerization(Player players[], int numPlayer);/*
+    Player players[]; array of players containing their scores and player_id
+    int numPlayer; number of players
+    Function prints the winner of the game or a tie
+*/
 void ShowBoard(Board* board);/*
     Board* board; pointer to the structure board(containing rows,columns,pointer to 2D matrix, penguins_per_player)
     Function prints the board to the screen
 */
-
 void FreeBoard(Board* board);/*
     Board* board; pointer to the structure board(containing rows,columns,pointer to 2D matrix, penguins_per_player)
     Function clears allocated memory for the dynamic 2D matrix(board)
