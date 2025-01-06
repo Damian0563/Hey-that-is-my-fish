d945d9d6c4e76a38f348b8d33d922b23ca31404f
Huynh Gia Bao Nguyen <h.nguyen@stud.elka.pw.edu.pl>
2025-01-06 17:42:23 +0100

    Merge branch 'Bao'sbranch' into 'main'
    Bao'sbranch
    
    See merge request hnguyen2/epfu24w-103-d-penguins!50
    
    (cherry picked from commit a29fe9c3bb664da7faf1a15fba6b8943c848eeab)
    
    0cba1e52 Add new directory
    89f75f26 Upload New File
    acb98324 Delete T5.zip
    e1ceb4c7 Upload New File
    673b14c4 Upload New File
    5979b7ab Upload New File
    7638359f Upload New File
    1d5a849d Upload New File
    dd5c7ada Upload New File
    471d49c0 Upload New File
    a75c232e Upload New File
    6750bc4c Upload New File
    43100b71 Upload New File
    1a814ea3 Upload New File
    965d962f Upload New File
    7428f8c8 Upload New File
    
    Co-authored-by: Huynh Gia Bao Nguyen <h.nguyen@stud.elka.pw.edu.pl>

---
+++ T5/main.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+#include "placement.h"
+#include "movement.h"
+#include "utils.h"
+#include "structs.h"
+#include "file_handling.h"
+
+int main(int argc, char *argv[])
+{
+    Board board;
+    AutonomousPlayer *players;
+    int num_players = 0;
+    if (argc == 1)
+    {
+        int numPlayer = 0;
+        numPlayer = askForPlayers(numPlayer);
+        Player players[numPlayer];
+        // Initialize players
+        initializePlayers(numPlayer, players);
+        srand(time(NULL));
+        int stuck = 0;
+        GenerateBoard(&board, numPlayer);
+        int curPlayerIndex = 0, total = board.penguins_per_player * numPlayer;
+        FillBoard(&board, numPlayer);
+        printf("\nGenerated board: \n");
+        ShowBoard(&board);
+
+        printf("\nPlacement phase commences\n\n");
+        do
+        {
+            int x, y;
+            int sign = curPlayerIndex + 6;
+            printf("\nPlayer %d place your penguin\n", curPlayerIndex + 1);
+            do
+            {
+                AskForCoordinates(&x, &y);
+            } while (ValidateCoordinates(&board, x, y) == 1);
+            PlacePenguin(&board, x, y, sign);
+            ShowBoard(&board);
+            curPlayerIndex = (curPlayerIndex + 1) % numPlayer;
+            total -= 1;
+        } while (total > 0);
+
+        // movement phase begins
+        printf("\nMovement phase commences\n");
+        do
+        {
+            int x, y, x1, y1;
+            int sign = curPlayerIndex + 6;
+            if (CheckStuck(&board, sign) == 1)
+            {
+                curPlayerIndex = (curPlayerIndex + 1) % numPlayer;
+                stuck++;
+            }
+            else
+            {
+                do
+                {
+                    AskForCoordinatesOfPenguin(&x, &y, sign, &board);
+                    AskForCoordinatesMovement(&x, &y, &x1, &y1, sign, &board);
+                } while (ValidateMove(&board, x, y, x1, y1) == 1);
+                players[curPlayerIndex].score += board.array[x1][y1];
+                MovePenguin(&board, &x, &y, &x1, &y1, sign);
+                ShowBoard(&board);
+                printScores(players, numPlayer);
+                curPlayerIndex = (curPlayerIndex + 1) % numPlayer;
+                stuck = 0;
+            }
+        } while (stuck < numPlayer);
+        printf("No player can move. Game Ending...\n");
+        // Summarization
+        printf("Summarization\n");
+        printScores(players, numPlayer);
+        summerization(players, numPlayer);
+        FreeBoard(&board);
+        return 0;
+    }
+    else if (argc == 5 && strcmp(argv[1], "phase=placement") == 0)
+    {
+        // autonomous placement phase
+
+        int PenguinsToPlace;
+        if (sscanf(argv[2], "penguins=%d", &PenguinsToPlace) != 1)
+        {
+            fprintf(stderr, "Invalid format for penguins argument");
+            return 3;
+        }
+        char *InputFileName = argv[3];
+        char *OutputFileName = argv[4];
+        ReadFile(&board, InputFileName, &players, &num_players);
+        ShowBoard(&board);
+        PlaceAutonomously(&board, PenguinsToPlace);
+        WriteFile(&board, OutputFileName, players, num_players);
+        FreeBoard(&board);
+        free(players);
+    }
+    else if (argc == 4 && strcmp(argv[1], "phase=movement") == 0)
+    {
+        // autonomous movement phase
+        AutonomousPlayer Players[9];
+        int num_players = 0;
+        char *InputFileName = argv[2];
+        char *OutputFileName = argv[3];
+        ReadFile(&board, InputFileName, &players, &num_players);
+        MoveAutonomously(&board);
+        WriteFile(&board, OutputFileName, players, num_players);
+        FreeBoard(&board);
+    }
+    else if (argc == 2 && strcmp(argv[1], "name") == 0)
+    {
+        char *extension = strrchr(argv[0], '.');
+        if (extension && strcmp(extension, ".exe") == 0)
+        {
+            *extension = '\0'; // Truncate the string at the ".exe"
+        }
+        printf("%s", basename(argv[0]));
+    }
+    else
+    {
+        printf("Invalid amount of command line arguments or arguments");
+        return 3;
+    }
+}
